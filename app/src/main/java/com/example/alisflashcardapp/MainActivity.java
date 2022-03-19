c9de10ca479ec15ab37b73b10a62af52b9a02ec7
doubleaspeaks <aliashrar569@gmail.com>
2022-03-18 20:06:34 -0400

    Second commit

--- app/src/main/java/com/example/alisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/alisflashcardapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.alisflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionTextView;
+    TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,30 @@ public class MainActivity extends AppCompatActivity {
                 flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_add_card.class);
+          //  startActivity(intent);
+            startActivityForResult(intent, 100);
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100) {
 
+            if (data != null) {
+               String questionString =  data.getExtras().getString("QUESTION_KEY");
+               String answerString = data.getExtras().getString("ANSWER_KEY");
+                questionTextView.setText(questionString);
+                answerTextView.setText(answerString);
+            }
+        }
+    }
 }
