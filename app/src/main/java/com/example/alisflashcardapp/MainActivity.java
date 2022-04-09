6ad4f7ac6e1f73917920bf043b38e4f53336f6c2
doubleaspeaks <aliashrar569@gmail.com>
2022-04-08 22:18:39 -0400

    5th commit

--- app/src/main/java/com/example/alisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/alisflashcardapp/MainActivity.java
@@ -6,27 +6,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView questionTextView;
     TextView answerTextView;
 
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int cardIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
-        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
-        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+        questionTextView = findViewById(R.id.flashcard_question);
+        answerTextView = findViewById(R.id.flashcard_answer);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                flashcardQuestion.setVisibility(View.INVISIBLE);
-                flashcardAnswer.setVisibility(View.VISIBLE);
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
             }
+
+
+
+
+
         });
 
         ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
@@ -37,9 +53,73 @@ public class MainActivity extends AppCompatActivity {
           //  startActivity(intent);
             startActivityForResult(intent, 100);
             }
+
+
+        });
+
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if(allFlashcards != null && allFlashcards.size() > 0) {
+            Flashcard firstcard = allFlashcards.get(0);
+            questionTextView.setText(firstcard.getQuestion());
+            answerTextView.setText(firstcard.getAnswer());
+        }
+
+        findViewById(R.id.flashcard_next_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardIndex *= 1;
+
+                if (cardIndex < allFlashcards.size()) {
+                    Snackbar.make(view,
+                           "You've reacher the end of the cards! Going back to the start.",
+                           Snackbar.LENGTH_SHORT)
+                    .show();
+
+                    cardIndex = 0; //resetting index
+                }
+
+
+
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.right_in);
+
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+
+                        questionTextView.startAnimation(rightInAnim);
+
+
+                        Flashcard currentcard = allFlashcards.get(cardIndex);
+                        questionTextView.setText(currentcard.getQuestion());
+                        answerTextView.setText(currentcard.getAnswer());
+
+                        questionTextView.setVisibility(View.VISIBLE);
+                        answerTextView.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
+                questionTextView.startAnimation(leftOutAnim);
+
+            }
         });
 
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -51,7 +131,17 @@ public class MainActivity extends AppCompatActivity {
                String answerString = data.getExtras().getString("ANSWER_KEY");
                 questionTextView.setText(questionString);
                 answerTextView.setText(answerString);
+
+               Flashcard flashcard = new Flashcard(questionString, answerString);
+               flashcardDatabase.insertCard(flashcard);
+
+
+               allFlashcards = flashcardDatabase.getAllCards();
+
             }
         }
     }
 }
+
+
+
